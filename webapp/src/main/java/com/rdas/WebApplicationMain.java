85acbed442ff4651c1035d7eb67225ccc0f23fe1
Rana Das <rana.pratap.das@gmail.com>
2019-08-31 22:30:10 +0100

    adding undertow

---
+++ webapp/src/main/java/com/rdas/WebApplicationMain.java
@@ -0,0 +1,13 @@
+package com.rdas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplicationMain {
+
+	public static void main(String [] args) {
+		SpringApplication.run(WebApplicationMain.class, args);
+	}
+}
+
