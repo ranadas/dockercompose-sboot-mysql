3b22db0c735acd35d15d2a3d5c60b0aff6fe2995
Rana Das <rana.pratap.das@gmail.com>
2019-09-01 13:46:24 +0100

    Adding ToDo Entity Definition

--- webapp/src/main/java/com/rdas/entity/Todo.java
+++ webapp/src/main/java/com/rdas/entity/Todo.java
@@ -2,6 +2,7 @@ package com.rdas.entity;
 
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -9,20 +10,25 @@ import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import lombok.Builder;
 import lombok.Data;
 
-@Entity
+@Builder
+@Entity(name = "todos")
 @Data
 public class Todo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "title", nullable = false)
     @NotNull
     @Size(min = 2, max = 100)
     private String title;
 
+    @Column(name = "completed", nullable = false)
     private boolean completed;
 
+    @Column(name = "updatedDate", nullable = false)
     private Date updatedDate = new Date();    
 }
