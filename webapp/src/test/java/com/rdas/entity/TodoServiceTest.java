74152bec7f29d147d098e2b6c621cc47c407ad96
Rana Das <rana.pratap.das@gmail.com>
2019-09-02 22:13:18 +0100

    Adding Service Mokito Tests

--- webapp/src/test/java/com/rdas/entity/TodoServiceTest.java
+++ webapp/src/test/java/com/rdas/entity/TodoServiceTest.java
@@ -1,52 +1,65 @@
 package com.rdas.entity;
 
-import lombok.extern.log4j.Log4j2;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.ActiveProfiles;
 
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
+import java.util.Optional;
 
-import static org.junit.Assert.*;
+import static org.assertj.core.api.Java6Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
-@Ignore
-@Log4j2
-@ActiveProfiles("test")
-//@RunWith(SpringRunner.class)
 @RunWith(MockitoJUnitRunner.class)
 public class TodoServiceTest {
-    @MockBean
-    TodoRepository todoRepository;
 
-//    @Mock
-//    TodoRepository todoRepository2;
+    @Mock
+    TodoRepository todoRepository;
 
     @InjectMocks
     TodoService todoService;
 
-    //@Spy
-    private List<Todo> todos = new ArrayList<>();
-    private Todo alex = Todo.builder().title("Add Alex").completed(false).updatedDate(new Date()).build();
-
-    //when(mockRepository.findById(1L)).thenReturn(Optional.of(book));
+    @Test
+    public void service_injected() {
+        assertThat(todoService).isNotNull();
+    }
 
     @Test
-    public void return_emptytods_at_start() {
-        assertNotNull(todoService);
-//        when(todoRepository.findAll()).thenReturn(todos);
-        when(todoRepository.findByTitle("Add Alex")).thenReturn(alex);
+    public void service_save() {
+        Todo todo = Todo.builder().id(1L).title("Add JaCoco to Codebase").completed(false).updatedDate(new Date()).build();
+        when(todoRepository.save(todo)).thenReturn(todo);
 
-        Todo found = todoService.getByTitle("Add Alex");
+        Todo saved = todoService.save(todo);
+        assertThat(saved.getTitle()).isEqualTo(todo.getTitle());
+    }
 
-        System.out.println(found);
+    @Test
+    public void service_save_retrieveById() {
+        Todo todo = Todo.builder().title("Add JaCoco to Codebase").completed(false).updatedDate(new Date()).build();
+        when(todoRepository.save(todo)).thenReturn(todo);
+        when(todoRepository.findById(1L)).thenReturn(Optional.of(todo));
+
+        Todo saved = todoService.save(todo);
+        assertThat(saved.getTitle()).isEqualTo(todo.getTitle());
+
+        Optional<Todo> byId = todoService.getById(1L);
+        assertThat(byId.isPresent()).isTrue();
+        assertThat(byId.get().getTitle()).isEqualTo(todo.getTitle());
+    }
+
+
+    @Test
+    public void service_save_retrieveByTitle() {
+        Todo todo = Todo.builder().title("Add JaCoco to Codebase").completed(false).updatedDate(new Date()).build();
+        when(todoRepository.save(todo)).thenReturn(todo);
+        when(todoRepository.findByTitle("Add JaCoco to Codebase")).thenReturn(todo);
+
+        Todo saved = todoService.save(todo);
+        assertThat(saved.getTitle()).isEqualTo(todo.getTitle());
+
+        Todo found = todoService.getByTitle("Add JaCoco to Codebase");
+        assertThat(found.getTitle()).isEqualTo(todo.getTitle());
     }
 }
