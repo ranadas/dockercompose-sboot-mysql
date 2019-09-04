b75fa2af62eb6d534f45be0ac26d26cdc2ea7fd8
Rana Das <rana.pratap.das@gmail.com>
2019-09-04 06:31:06 +0100

    Add a new test case to check view name

--- webapp/src/test/java/com/rdas/resources/IndexControllerTest.java
+++ webapp/src/test/java/com/rdas/resources/IndexControllerTest.java
@@ -9,15 +9,14 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.Assert.*;
-import static org.junit.Assert.assertEquals;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(IndexController.class)
-//@Import(SpringSecurityConfig.class)
+//@Import(SomeConfig.class)
 public class IndexControllerTest {
     @Autowired
     private MockMvc mockMvc;
@@ -26,16 +25,22 @@ public class IndexControllerTest {
     private ObjectMapper objectMapper;
 
     @Test
-    public void testCreateUser1() throws Exception {
+    public void test_homepage_returns_ok() throws Exception {
         mockMvc.perform(get("/")
-                .accept(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
                 //.with(httpBasic(username, password))
-                )
+        )
                 .andExpect(status().isOk())
-                //.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(content().contentType("text/html;charset=UTF-8"))
 //                .andExpect(jsonPath("$.user_id", CoreMatchers.is(userId)))
 //                .andExpect(jsonPath("$.name", CoreMatchers.is(name)))
 //                .andExpect(jsonPath("$.username", CoreMatchers.is(username)))
-            ;
+        ;
+    }
+    @Test
+    public void test_homepage_returns_index() throws Exception {
+        mockMvc.perform(get("/"))
+                .andDo(print())
+                .andExpect(view().name("index"));
     }
 }
